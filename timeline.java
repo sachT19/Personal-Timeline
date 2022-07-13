24c9e00bab53fa1b87e70972478535008d4ce3e8
Sachleen <97253957+sachT19@users.noreply.github.com>
2022-07-13 23:01:17 +0300

    Create timeline.java

---
+++ timeline.java
@@ -0,0 +1,29 @@
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        // Start here!
+        System.out.println("Age    | M/YY - Description");
+        System.out.println("7      | 7/07 - First trip to Disney World");
+        System.out.println("       |");
+        System.out.println("       |");
+        System.out.println("       |");
+        
+        System.out.println("9      | 9/09 - Went to India");
+        System.out.println("       | 10/09 - Winter break");
+        System.out.println("       |");
+        System.out.println("       |");
+        
+        System.out.println("11     | 11/11 - School @ Quest Academy");
+        System.out.println("       | 12/11 - Winter Break");
+        System.out.println("       |");
+        System.out.println("       |");
+        
+        System.out.println("13     | 9/13 - Middle School");
+        System.out.println("       |");
+        System.out.println("       |");
+        System.out.println("       |");
+        
+        
+    }
+}
